7237a6551b31cf8f5c0525672eadc595f44bbf31
KelvinMitaki <mitakikelvin1@gmail.com>
2021-02-18 22:38:34 +0300

    wired up screens

--- App.tsx
+++ App.tsx
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { enableScreens } from "react-native-screens";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import SettingsScreen from "./screens/SettingsScreen";
+import MapScreen from "./screens/MapScreen";
+import DeckScreen from "./screens/DeckScreen";
+import ReviewScreen from "./screens/ReviewScreen";
+import AuthScreen from "./screens/AuthScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+enableScreens();
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const StackNavigator = createStackNavigator({
+  Settings: SettingsScreen
 });
+const BottomTabNavigator = createBottomTabNavigator({
+  Map: MapScreen,
+  Deck: DeckScreen,
+  Review: ReviewScreen,
+  Settings: StackNavigator
+});
+const SwitchNavigator = createSwitchNavigator({
+  Auth: AuthScreen,
+  Welcome: WelcomeScreen,
+  Main: BottomTabNavigator
+});
+
+const App = createAppContainer(SwitchNavigator);
+
+export default () => <App />;
