56112f7de97a023dac505e0b136a1d1247d611d5
KelvinMitaki <mitakikelvin1@gmail.com>
2021-02-18 22:48:13 +0300

    added header to all screens

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { LogBox, StyleSheet, Text, View } from "react-native";
 import { enableScreens } from "react-native-screens";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createStackNavigator } from "react-navigation-stack";
@@ -11,19 +11,25 @@ import ReviewScreen from "./screens/ReviewScreen";
 import AuthScreen from "./screens/AuthScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 enableScreens();
-
+LogBox.ignoreLogs([
+  "It appears that you are using old version of react-navigation library. Please update @react-navigation/bottom-tabs, @react-navigation/stack and @react-navigation/drawer to version 5.10.0 or above to take full advantage of new functionality added to react-native-screens"
+]);
 const StackNavigator = createStackNavigator({
   Settings: SettingsScreen
 });
 const BottomTabNavigator = createBottomTabNavigator({
-  Map: MapScreen,
-  Deck: DeckScreen,
-  Review: ReviewScreen,
-  Settings: StackNavigator
+  Map: createStackNavigator({ Map: MapScreen }),
+  Deck: createStackNavigator({ Deck: DeckScreen }),
+  Review: createStackNavigator({
+    Review: ReviewScreen,
+    Settings: StackNavigator
+  })
 });
 const SwitchNavigator = createSwitchNavigator({
-  Auth: AuthScreen,
-  Welcome: WelcomeScreen,
+  Auth: createStackNavigator({
+    Welcome: WelcomeScreen,
+    Auth: AuthScreen
+  }),
   Main: BottomTabNavigator
 });
 
