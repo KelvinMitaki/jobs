ee6154861311ea6f7230a15f530c07051a6723a4
KelvinMitaki <mitakikelvin1@gmail.com>
2021-02-19 23:08:57 +0300

    showed map on screen

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
 
 const MapScreen = () => {
   return (
-    <View>
-      <Text>MapScreen MapScreen</Text>
+    <View style={{ flex: 1 }}>
+      <MapView style={{ flex: 1 }} />
     </View>
   );
 };
